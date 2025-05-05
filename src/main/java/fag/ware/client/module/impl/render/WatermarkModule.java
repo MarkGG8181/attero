719de937aa529bdd50194643ad9862c8f40b2678
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-05-05 13:39:21 +0300

    color setting test

--- src/main/java/fag/ware/client/module/impl/render/WatermarkModule.java
+++ src/main/java/fag/ware/client/module/impl/render/WatermarkModule.java
@@ -5,14 +5,17 @@ import fag.ware.client.event.impl.render.Render2DEvent;
 import fag.ware.client.module.Module;
 import fag.ware.client.module.data.ModuleCategory;
 import fag.ware.client.module.data.ModuleInfo;
+import fag.ware.client.module.data.setting.impl.ColorSetting;
 
 import java.awt.*;
 
 @ModuleInfo(name = "Watermark", category = ModuleCategory.RENDER, description = "Draws a watermark")
 public class WatermarkModule extends Module {
+    private final ColorSetting color = new ColorSetting("Color", new Color(255, 255, 255));
+
     @Subscribe
     public void onRender(Render2DEvent event) {
-        event.getDrawContext().drawText(mc.textRenderer, "Fag", 5, 5, Color.WHITE.getRGB(), false);
+        event.getDrawContext().drawText(mc.textRenderer, "Fag", 5, 5, color.toInt(), false);
     }
 
     @Override
