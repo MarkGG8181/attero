ca1f05c256121c6a7f960e89eaa135a742df23c9
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-05-05 13:14:09 +0300

    more test settings

--- src/main/java/fag/ware/client/module/impl/combat/KillAuraModule.java
+++ src/main/java/fag/ware/client/module/impl/combat/KillAuraModule.java
@@ -4,10 +4,12 @@ import fag.ware.client.module.Module;
 import fag.ware.client.module.data.ModuleCategory;
 import fag.ware.client.module.data.ModuleInfo;
 import fag.ware.client.module.data.setting.impl.NumberSetting;
+import fag.ware.client.module.data.setting.impl.StringSetting;
 
 @ModuleInfo(name = "KillAura", category = ModuleCategory.COMBAT, description = "Attacks entities in close proximity")
 public class KillAuraModule extends Module {
     private final NumberSetting range = new NumberSetting("Range", 3, 1, 6);
+    private final StringSetting sortBy = new StringSetting("Sort by", "Range", "Range", "Health", "Armor", "Hurt-ticks");
 
     @Override
     public void onEnable() {
--- src/main/java/fag/ware/client/module/impl/render/AntiBlindModule.java
+++ src/main/java/fag/ware/client/module/impl/render/AntiBlindModule.java
@@ -5,37 +5,45 @@ import fag.ware.client.event.impl.render.*;
 import fag.ware.client.module.Module;
 import fag.ware.client.module.data.ModuleCategory;
 import fag.ware.client.module.data.ModuleInfo;
+import fag.ware.client.module.data.setting.impl.BooleanSetting;
 
 @ModuleInfo(name = "AntiBlind", category = ModuleCategory.RENDER, description = "Prevents your view from being obstructed")
 public class AntiBlindModule extends Module {
+    private final BooleanSetting fireOverlay = new BooleanSetting("Fire overlay", true);
+    private final BooleanSetting waterOverlay = new BooleanSetting("Water overlay", true);
+    private final BooleanSetting wallOverlay = new BooleanSetting("Wall overlay", true);
+    private final BooleanSetting portalOverlay = new BooleanSetting("Portal overlay", true);
+    private final BooleanSetting vignetteOverlay = new BooleanSetting("Vignette overlay", true);
+    private final BooleanSetting nauseaOverlay = new BooleanSetting("Nausea overlay", true);
+
     @Subscribe
     public void onRenderFireOverlay(RenderFireOverlayEvent event) {
-        event.setCancelled(true);
+        event.setCancelled(fireOverlay.getValue());
     }
 
     @Subscribe
     public void onRenderUnderwaterOverlay(RenderUnderwaterOverlayEvent event) {
-        event.setCancelled(true);
+        event.setCancelled(waterOverlay.getValue());
     }
 
     @Subscribe
     public void onRenderInWallOverlay(RenderInWallOverlayEvent event) {
-        event.setCancelled(true);
+        event.setCancelled(wallOverlay.getValue());
     }
 
     @Subscribe
     public void onRenderPortalOverlay(RenderPortalOverlayEvent event) {
-        event.setCancelled(true);
+        event.setCancelled(portalOverlay.getValue());
     }
 
     @Subscribe
     public void onRenderVignetteOverlay(RenderVignetteOverlayEvent event) {
-        event.setCancelled(true);
+        event.setCancelled(vignetteOverlay.getValue());
     }
 
     @Subscribe
     public void onRenderNauseaOverlay(RenderNauseaOverlayEvent event) {
-        event.setCancelled(true);
+        event.setCancelled(nauseaOverlay.getValue());
     }
 
     @Override
