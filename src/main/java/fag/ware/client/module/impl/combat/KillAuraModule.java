fd6f2371cba53ca47730bc59e7434f7505c4dde1
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-05-05 12:31:34 +0300

    test settings

--- src/main/java/fag/ware/client/module/impl/combat/KillAuraModule.java
+++ src/main/java/fag/ware/client/module/impl/combat/KillAuraModule.java
@@ -3,9 +3,12 @@ package fag.ware.client.module.impl.combat;
 import fag.ware.client.module.Module;
 import fag.ware.client.module.data.ModuleCategory;
 import fag.ware.client.module.data.ModuleInfo;
+import fag.ware.client.module.data.setting.impl.NumberSetting;
 
 @ModuleInfo(name = "KillAura", category = ModuleCategory.COMBAT, description = "Attacks entities in close proximity")
 public class KillAuraModule extends Module {
+    private final NumberSetting range = new NumberSetting("Range", 3, 1, 6);
+
     @Override
     public void onEnable() {
 
