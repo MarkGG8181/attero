da4304000c0f6cc710572f23d9fd35b4ed8b106c
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-05-05 17:52:09 +0300

    Update KillAuraModule.java

--- src/main/java/fag/ware/client/module/impl/combat/KillAuraModule.java
+++ src/main/java/fag/ware/client/module/impl/combat/KillAuraModule.java
@@ -8,8 +8,8 @@ import fag.ware.client.module.data.setting.impl.StringSetting;
 
 @ModuleInfo(name = "KillAura", category = ModuleCategory.COMBAT, description = "Attacks entities in close proximity")
 public class KillAuraModule extends Module {
-    private final NumberSetting range = new NumberSetting("Range", 3, 1, 6);
     private final StringSetting sortBy = new StringSetting("Sort by", "Range", "Range", "Health", "Armor", "Hurt-ticks");
+    private final NumberSetting range = new NumberSetting("Range", 3, 1, 6);
 
     @Override
     public void onEnable() {
