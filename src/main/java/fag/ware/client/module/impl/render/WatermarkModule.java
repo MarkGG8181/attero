9e9010efd2c3b27373d8e5c13de069f3bfb22143
Mainline <igobylotsofnames@keemail.me>
2025-05-09 23:37:22 +0200

    minecraft design for watermark

--- src/main/java/fag/ware/client/module/impl/render/WatermarkModule.java
+++ src/main/java/fag/ware/client/module/impl/render/WatermarkModule.java
@@ -6,6 +6,7 @@ import fag.ware.client.module.Module;
 import fag.ware.client.module.data.ModuleCategory;
 import fag.ware.client.module.data.ModuleInfo;
 import fag.ware.client.module.data.setting.impl.ColorSetting;
+import fag.ware.client.module.data.setting.impl.StringSetting;
 import fag.ware.client.screen.ClickScreen;
 import fag.ware.client.util.imgui.ImGuiImpl;
 import imgui.ImDrawList;
@@ -16,6 +17,7 @@ import java.awt.*;
 
 @ModuleInfo(name = "Watermark", category = ModuleCategory.RENDER, description = "Draws a watermark")
 public class WatermarkModule extends Module {
+    private final StringSetting mode = new StringSetting("Design", "Rounded", "Rounded", "Minecraft");
     private final ColorSetting color = new ColorSetting("Color", new Color(0x26A07D));
 
     @Subscribe
@@ -24,31 +26,39 @@ public class WatermarkModule extends Module {
             return;
         }
 
-        ImGuiImpl.draw(io -> {
-            ImGui.pushFont(ImGuiImpl.INTER_REGULAR_17);
-            ImDrawList drawList = ImGui.getForegroundDrawList();
+        if (mode.is("Rounded"))
+        {
+            ImGuiImpl.draw(io -> {
+                ImGui.pushFont(ImGuiImpl.INTER_REGULAR_17);
+                ImDrawList drawList = ImGui.getForegroundDrawList();
 
-            String watermarkText = "Fagware";
-            String userInfoText = "| " + mc.getSession().getUsername() + " | " + mc.getCurrentFps() + " fps";
+                String watermarkText = "Fagware";
+                String userInfoText = "| " + mc.getSession().getUsername() + " | " + mc.getCurrentFps() + " fps";
 
-            ImVec2 watermarkTextSize = ImGui.calcTextSize(watermarkText);
-            ImVec2 userInfoTextSize = ImGui.calcTextSize(userInfoText);
+                ImVec2 watermarkTextSize = ImGui.calcTextSize(watermarkText);
+                ImVec2 userInfoTextSize = ImGui.calcTextSize(userInfoText);
 
-            float x = 10;
-            float y = 10;
+                float x = 10;
+                float y = 10;
 
-            float width = watermarkTextSize.x + userInfoTextSize.x + 25;
-            float height = 30;
+                float width = watermarkTextSize.x + userInfoTextSize.x + 25;
+                float height = 30;
 
-            drawList.addRectFilled(x, y, x + width, y + height, toImGuiColor(new Color(0, 0, 0, 150)), 6.0f);
+                drawList.addRectFilled(x, y, x + width, y + height, toImGuiColor(new Color(0, 0, 0, 150)), 6.0f);
 
-            drawList.addText(x + 10, y + 4, color.toImGuiColor(), watermarkText.substring(0, 3));
-            drawList.addText(x + 10 + ImGui.calcTextSize(watermarkText.substring(0, 3)).x, y + 4, toImGuiColor(Color.WHITE), watermarkText.substring(3));
+                drawList.addText(x + 10, y + 4, color.toImGuiColor(), watermarkText.substring(0, 3));
+                drawList.addText(x + 10 + ImGui.calcTextSize(watermarkText.substring(0, 3)).x, y + 4, toImGuiColor(Color.WHITE), watermarkText.substring(3));
 
-            drawList.addText(x + 5 + watermarkTextSize.x + 10, y + 4, toImGuiColor(Color.WHITE), userInfoText);
+                drawList.addText(x + 5 + watermarkTextSize.x + 10, y + 4, toImGuiColor(Color.WHITE), userInfoText);
 
-            ImGui.popFont();
-        });
+                ImGui.popFont();
+            });
+        }
+        else if (mode.is("Minecraft"))
+        {
+            event.getDrawContext().drawText(mc.textRenderer, "fag", 5, 5, color.getValue().getRGB(), false);
+            event.getDrawContext().drawText(mc.textRenderer, "ware", 5 + mc.textRenderer.getWidth("fag"), 5, -1, false);
+        }
     }
 
     @Override
