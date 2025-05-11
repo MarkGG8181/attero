9896a51986cb14e0124733012e2215868b85e6e9
knezoWTF <206253445+knezoWTF@users.noreply.github.com>
2025-05-11 19:56:28 +0200

    idk what this is dawg

--- src/main/java/fag/ware/client/tracker/impl/ModuleTracker.java
+++ src/main/java/fag/ware/client/tracker/impl/ModuleTracker.java
@@ -6,6 +6,7 @@ import fag.ware.client.event.impl.KeyEvent;
 import fag.ware.client.module.Module;
 import fag.ware.client.module.data.ModuleCategory;
 import fag.ware.client.module.impl.combat.KillAuraModule;
+import fag.ware.client.module.impl.combat.VelocityModule;
 import fag.ware.client.module.impl.movement.CorrectMovementModule;
 import fag.ware.client.module.impl.movement.SpeedModule;
 import fag.ware.client.module.impl.movement.SprintModule;
@@ -31,7 +32,7 @@ public class ModuleTracker extends Tracker<Module> {
         getSet().add(new WatermarkModule());
         getSet().add(new SpeedModule());
         getSet().add(new CorrectMovementModule());
-
+        getSet().add(new VelocityModule());
         getSet().forEach(Module::onInit);
     }
 
