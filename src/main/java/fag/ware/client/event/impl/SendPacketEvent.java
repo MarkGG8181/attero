0de99923210818a6394b7128de7d6ebed336ba0b
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-05-12 18:00:24 +0300

    @Getter for packet events

--- src/main/java/fag/ware/client/event/impl/SendPacketEvent.java
+++ src/main/java/fag/ware/client/event/impl/SendPacketEvent.java
@@ -2,9 +2,11 @@ package fag.ware.client.event.impl;
 
 import fag.ware.client.event.CancellableEvent;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import net.minecraft.network.packet.Packet;
 
 @AllArgsConstructor
+@Getter
 public class SendPacketEvent extends CancellableEvent {
     private final Packet<?> packet;
 }
