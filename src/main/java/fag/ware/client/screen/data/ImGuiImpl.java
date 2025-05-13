c28d900f2eb44fa7fafcbbe44c0ea1b3f8c34de8
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-05-14 00:17:18 +0300

    Update ImGuiImpl.java

--- src/main/java/fag/ware/client/screen/data/ImGuiImpl.java
+++ src/main/java/fag/ware/client/screen/data/ImGuiImpl.java
@@ -18,11 +18,14 @@ import net.minecraft.client.gl.Framebuffer;
 import net.minecraft.client.gl.GlBackend;
 import net.minecraft.client.texture.GlTexture;
 import org.apache.commons.io.IOUtils;
+import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL30;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -115,37 +118,36 @@ public class ImGuiImpl {
         ImPlot.destroyContext();
     }
 
-// Can be used to load buffered images in ImGui
-//    public static int fromBufferedImage(BufferedImage image) {
-//        final int[] pixels = new int[image.getWidth() * image.getHeight()];
-//        image.getRGB(0, 0, image.getWidth(), image.getHeight(), pixels, 0, image.getWidth());
-//
-//        final ByteBuffer buffer = BufferUtils.createByteBuffer(image.getWidth() * image.getHeight() * 4);
-//
-//        for (int y = 0; y < image.getHeight(); y++) {
-//            for (int x = 0; x < image.getWidth(); x++) {
-//                final int pixel = pixels[y * image.getWidth() + x];
-//
-//                buffer.put((byte) ((pixel >> 16) & 0xFF));
-//                buffer.put((byte) ((pixel >> 8) & 0xFF));
-//                buffer.put((byte) (pixel & 0xFF));
-//                buffer.put((byte) ((pixel >> 24) & 0xFF));
-//            }
-//        }
-//
-//        buffer.flip();
-//
-//        final int texture = GlStateManager._genTexture();
-//        GlStateManager._bindTexture(texture);
-//
-//        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
-//        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
-//
-//        GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA8, image.getWidth(), image.getHeight(), 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, buffer);
-//
-//        return texture;
-//    }
+    // Can be used to load buffered images in ImGui
+    public static int fromBufferedImage(BufferedImage image) {
+        final int[] pixels = new int[image.getWidth() * image.getHeight()];
+        image.getRGB(0, 0, image.getWidth(), image.getHeight(), pixels, 0, image.getWidth());
 
+        final ByteBuffer buffer = BufferUtils.createByteBuffer(image.getWidth() * image.getHeight() * 4);
+
+        for (int y = 0; y < image.getHeight(); y++) {
+            for (int x = 0; x < image.getWidth(); x++) {
+                final int pixel = pixels[y * image.getWidth() + x];
+
+                buffer.put((byte) ((pixel >> 16) & 0xFF));
+                buffer.put((byte) ((pixel >> 8) & 0xFF));
+                buffer.put((byte) (pixel & 0xFF));
+                buffer.put((byte) ((pixel >> 24) & 0xFF));
+            }
+        }
+
+        buffer.flip();
+
+        final int texture = GlStateManager._genTexture();
+        GlStateManager._bindTexture(texture);
+
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+
+        GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA8, image.getWidth(), image.getHeight(), 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, buffer);
+
+        return texture;
+    }
 
     public static void applyMarineTheme() {
         ImGuiStyle style = ImGui.getStyle();
