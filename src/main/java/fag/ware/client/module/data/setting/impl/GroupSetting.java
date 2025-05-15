06b676f3dea8d475937f0893082fb9084c3ca821
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-05-15 16:02:49 +0300

    Update GroupSetting.java

--- src/main/java/fag/ware/client/module/data/setting/impl/GroupSetting.java
+++ src/main/java/fag/ware/client/module/data/setting/impl/GroupSetting.java
@@ -3,7 +3,7 @@ package fag.ware.client.module.data.setting.impl;
 import fag.ware.client.module.data.setting.AbstractSetting;
 
 public class GroupSetting extends AbstractSetting<Boolean> {
-    public GroupSetting(String name, boolean value) {
-        super(name, value);
+    public GroupSetting(String name, boolean expanded) {
+        super(name, expanded);
     }
 }
