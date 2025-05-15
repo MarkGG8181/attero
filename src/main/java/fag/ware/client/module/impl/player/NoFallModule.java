f0f74f129fb311957b865c389f9c79d572c6bbd3
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-05-15 13:08:21 +0300

    add more packet no falls

--- src/main/java/fag/ware/client/module/impl/player/NoFallModule.java
+++ src/main/java/fag/ware/client/module/impl/player/NoFallModule.java
@@ -2,7 +2,6 @@ package fag.ware.client.module.impl.player;
 
 import fag.ware.client.event.data.Subscribe;
 import fag.ware.client.event.impl.MotionEvent;
-import fag.ware.client.event.impl.TickEvent;
 import fag.ware.client.module.AbstractModule;
 import fag.ware.client.module.data.ModuleCategory;
 import fag.ware.client.module.data.ModuleInfo;
@@ -13,18 +12,24 @@ import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 
 @ModuleInfo(name = "NoFall", category = ModuleCategory.PLAYER, description = "Makes you take 0 fall damage")
 public class NoFallModule extends AbstractModule {
-    private final StringSetting mode = new StringSetting("Mode", "Damage", "Damage", "Vanilla", "Verus");
-    private final NumberSetting falldistance = new NumberSetting("Distance", 3.35, 1, 5);
+    private final StringSetting mode = new StringSetting("Mode", "Vanilla", "Vanilla", "Verus");
+    private final StringSetting vanillaMode = (StringSetting) new StringSetting("Vanilla mode", "Packet-Full", "Packet-Full", "Packet-OnGround", "Set ground").hide(() -> !mode.is("Vanilla"));
+    private final NumberSetting distance = new NumberSetting("Distance", 3.35, 1, 5);
 
     @Subscribe
     public void onTick(MotionEvent event) {
-        if (event.isPre() && mc.player.fallDistance >= falldistance.toFloat()) {
+        if (event.isPre() && mc.player.fallDistance >= distance.toFloat()) {
             switch (mode.getValue()) {
-                case "Damage" -> {
-                    mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.OnGroundOnly(true, false));
+                case "Vanilla" -> {
+                    switch (vanillaMode.getValue()) {
+                        case "Packet-OnGround" ->
+                                mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.OnGroundOnly(true, false));
+                        case "Packet-Full" ->
+                                mc.player.clientWorld.sendPacket(new PlayerMoveC2SPacket.Full(mc.player.getX(), mc.player.getY(), mc.player.getZ(), mc.player.getYaw(), mc.player.getPitch(), true, mc.player.horizontalCollision));
+                        case "Set ground" -> mc.player.setOnGround(true);
+                    }
                     mc.player.fallDistance = 0;
                 }
-                case "Vanilla" -> mc.player.setOnGround(true);
                 case "Verus" -> {
                     mc.player.setOnGround(true);
                     MovementUtil.setMotionY(0.0);
