f96b280f3812f44876a6175a99b90d7e6c4372c2
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-05-15 12:50:47 +0300

    add delay to autorespawn & auto log

--- src/main/java/fag/ware/client/module/impl/misc/AutoRespawnModule.java
+++ src/main/java/fag/ware/client/module/impl/misc/AutoRespawnModule.java
@@ -5,14 +5,20 @@ import fag.ware.client.event.impl.TickEvent;
 import fag.ware.client.module.AbstractModule;
 import fag.ware.client.module.data.ModuleCategory;
 import fag.ware.client.module.data.ModuleInfo;
+import fag.ware.client.module.data.setting.impl.NumberSetting;
+import fag.ware.client.util.math.Timer;
 
 @ModuleInfo(name = "AutoRespawn", category = ModuleCategory.MISC, description = "Respawns automaticslly on death")
 public class AutoRespawnModule extends AbstractModule {
+    private final NumberSetting delay = new NumberSetting("Delay", 30, 0, 300);
+
+    private final Timer timer = new Timer();
+
     @Subscribe
     public void onTick(TickEvent event) {
         if (mc.player == null || mc.world == null) return;
 
-        if (mc.player.isDead()) {
+        if (mc.player.isDead() && timer.hasElapsed(delay.toInt(), true)) {
             mc.player.requestRespawn();
         }
     }
@@ -23,5 +29,6 @@ public class AutoRespawnModule extends AbstractModule {
 
     @Override
     public void onDisable() {
+        timer.reset();
     }
 }
--- src/main/java/fag/ware/client/module/impl/player/AutoLogModule.java
+++ src/main/java/fag/ware/client/module/impl/player/AutoLogModule.java
@@ -6,6 +6,7 @@ import fag.ware.client.module.data.ModuleCategory;
 import fag.ware.client.module.data.ModuleInfo;
 import fag.ware.client.module.data.setting.impl.NumberSetting;
 import fag.ware.client.module.AbstractModule;
+import fag.ware.client.util.math.Timer;
 import net.minecraft.network.packet.s2c.common.DisconnectS2CPacket;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
@@ -13,28 +14,18 @@ import net.minecraft.text.Text;
 @ModuleInfo(name = "AutoLog", category = ModuleCategory.PLAYER, description = "Automatically logs out if you're low on hp.")
 public class AutoLogModule extends AbstractModule {
     private final NumberSetting hp = new NumberSetting("Min HP", 5, 1, 20);
+    private final NumberSetting delay = new NumberSetting("Delay", 30, 0, 300);
 
-    // Horrid.
+    private final Timer timer = new Timer();
+
     @Subscribe
     public void onTick(TickEvent event) {
         if (mc.player == null || mc.world == null || mc.player.isDead()) return;
+        if (mc.player.getHealth() >= hp.getValue().floatValue() || !timer.hasElapsed(delay.toInt(), true)) return;
 
-        if (mc.player.getHealth() < hp.getValue().floatValue()) {
-            disconnect("Low HP");
-        }
-    }
-
-    private void disconnect(Object reason) {
         toggle();
-        MutableText text = Text.literal("[AutoLog] ");
-
-        if (reason instanceof String) {
-            text.append(Text.literal((String) reason));
-        } else if (reason instanceof Text) {
-            text.append((Text) reason);
-        }
-
-        mc.execute(() -> mc.player.networkHandler.onDisconnect(new DisconnectS2CPacket(text)));
+        Text reason = Text.literal("[AutoLog] Low HP");
+        mc.execute(() -> mc.player.networkHandler.onDisconnect(new DisconnectS2CPacket(reason)));
     }
 
     @Override
@@ -43,5 +34,6 @@ public class AutoLogModule extends AbstractModule {
 
     @Override
     public void onDisable() {
+        timer.reset();
     }
 }
