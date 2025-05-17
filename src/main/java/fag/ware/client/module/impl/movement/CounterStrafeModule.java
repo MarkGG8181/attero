24c6283628711f71235c2ac254673e07b0261400
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-05-17 11:58:01 +0300

    Counter-Strafe -> CounterStrafe

--- src/main/java/fag/ware/client/module/impl/movement/CounterStrafeModule.java
+++ src/main/java/fag/ware/client/module/impl/movement/CounterStrafeModule.java
@@ -8,13 +8,12 @@ import fag.ware.client.module.data.ModuleInfo;
 import fag.ware.client.util.game.MovementUtil;
 
 @SuppressWarnings("ALL")
-@ModuleInfo(name = "Counter-Strafe", category = ModuleCategory.MOVEMENT, description = "CS:GO Counterstrafing")
+@ModuleInfo(name = "CounterStrafe", category = ModuleCategory.MOVEMENT, description = "CS:GO Counterstrafing")
 public class CounterStrafeModule extends AbstractModule {
     @Subscribe
     public void Tick(TickEvent event) {
-        if (mc.player == null || mc.world == null || mc.currentScreen != null) {
-            return;
-        }
+        if (mc.player == null || mc.world == null) return;
+
         if (mc.player.isOnGround() && !MovementUtil.isMoving()) {
             MovementUtil.setMotionX(0);
             MovementUtil.setMotionZ(0);
