2a1fd14b88ad2ce78cd73a5e27957b926a69da4c
knezoWTF <206253445+knezoWTF@users.noreply.github.com>
2025-05-17 14:44:54 +0200

    thing

--- src/main/java/fag/ware/client/module/impl/combat/AutoTotemModule.java
+++ src/main/java/fag/ware/client/module/impl/combat/AutoTotemModule.java
@@ -5,6 +5,7 @@ import fag.ware.client.event.impl.TickEvent;
 import fag.ware.client.module.AbstractModule;
 import fag.ware.client.module.data.ModuleCategory;
 import fag.ware.client.module.data.ModuleInfo;
+import fag.ware.client.module.data.setting.impl.BooleanSetting;
 import fag.ware.client.module.data.setting.impl.NumberSetting;
 import fag.ware.client.util.math.Timer;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
@@ -14,8 +15,9 @@ import net.minecraft.screen.slot.SlotActionType;
 
 @ModuleInfo(name = "AutoTotem", category = ModuleCategory.COMBAT, description = "Automatically puts a totem in your offhand")
 public class AutoTotemModule extends AbstractModule {
-    private final NumberSetting inventoryOpenDelay = new NumberSetting("Inventory Open Delay", 60, 0, 750);
-    private final NumberSetting totemDelay = new NumberSetting("Totem Delay", 100, 0, 750);
+    private final BooleanSetting manuallyOpenInv = new BooleanSetting("Manually open inventory", false);
+    private final NumberSetting inventoryOpenDelay = (NumberSetting) new NumberSetting("Inventory Open Delay", 60, 0, 750).hide(() -> manuallyOpenInv.getValue());
+    private final NumberSetting totemDelay = (NumberSetting) new NumberSetting("Totem Delay", 100, 0, 750);
     private final Timer timer = new Timer();
 
     @Subscribe
@@ -34,7 +36,7 @@ public class AutoTotemModule extends AbstractModule {
             if (item.getItem() == Items.TOTEM_OF_UNDYING) {
                 int slot = i < 9 ? i + 36 : i;
 
-                if (!(mc.currentScreen instanceof InventoryScreen) && timer.hasElapsed(inventoryOpenDelay.toInt(), true)) {
+                if (!(mc.currentScreen instanceof InventoryScreen) && timer.hasElapsed(inventoryOpenDelay.toInt(), true) && !manuallyOpenInv.getValue()) {
                     mc.setScreen(new InventoryScreen(mc.player));
                 }
 
