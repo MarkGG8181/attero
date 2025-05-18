99afeb61de76d018b17dada1e4516fe4c4ced717
Mainline <igobylotsofnames@keemail.me>
2025-05-18 14:55:09 +0200

    oh fuck

--- src/main/java/fag/ware/client/command/impl/GarbageCollectorCommand.java
+++ src/main/java/fag/ware/client/command/impl/GarbageCollectorCommand.java
@@ -1,7 +1,9 @@
 package fag.ware.client.command.impl;
 
 import fag.ware.client.command.AbstractCommand;
+import fag.ware.client.command.data.CommandInfo;
 
+@CommandInfo(name = "Garbage collector", description = "Runs the java garbage collector", aliases = {"gc", "garbagecollect", "gcol"})
 public class GarbageCollectorCommand extends AbstractCommand
 {
     @Override
