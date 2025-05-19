bc97e0c55708911f5abfbb20acf8b0d5737f9546
mark <86848614+MarkGG8181@users.noreply.github.com>
2025-05-19 14:27:10 +0300

    Update FastUseModule.java

--- src/main/java/fag/ware/client/module/impl/player/FastUseModule.java
+++ src/main/java/fag/ware/client/module/impl/player/FastUseModule.java
@@ -12,43 +12,39 @@ import fag.ware.client.util.math.Timer;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import org.lwjgl.glfw.GLFW;
 
 @ModuleInfo(name = "FastUse", category = ModuleCategory.PLAYER, description = "Makes item use faster")
 public class FastUseModule extends AbstractModule {
-    public final NumberSetting delay = new NumberSetting("Delay", 100, 0, 750);
+    private final NumberSetting delay = new NumberSetting("Delay", 100, 0, 750);
     private final BooleanSetting blocks = new BooleanSetting("Blocks", true);
     private final BooleanSetting xp = new BooleanSetting("XP", true);
-    private final BooleanSetting legitClicking = new BooleanSetting("Legit Clicking", true);
-    Timer timer = new Timer();
-    private boolean should;
+    private final BooleanSetting legitClicking = new BooleanSetting("Legit clicking", false);
+
+    private final Timer timer = new Timer();
 
     @Subscribe
     public void onTick(TickEvent event) {
-        // mc.player.isHolding(Items.BOW)
         if (mc.player == null || mc.world == null || mc.player.isUsingItem()) return;
 
-        should = holdingItems();
-
+        boolean should = holdingItems();
         if (!delay()) return;
 
         if (should) {
             if (!legitClicking.getValue()) {
                 ((MinecraftClientAccessor) mc).setItemUseCooldown(0);
-            if (legitClicking.getValue()) {
-                ((MinecraftClientAccessor) mc).invokeDoItemUse();
-               }
+                if (legitClicking.getValue()) {
+                    ((MinecraftClientAccessor) mc).invokeDoItemUse();
+                }
             }
         }
     }
 
-    public boolean holdingItems() {
+    private boolean holdingItems() {
         ItemStack itemstack = mc.player.getMainHandStack();
         return mc.player.isHolding(Items.EXPERIENCE_BOTTLE) && xp.getValue() || blocks.getValue() && itemstack.getItem() instanceof BlockItem;
     }
 
-
-    public boolean delay() {
+    private boolean delay() {
         return timer.hasElapsed(delay.toInt(), true);
     }
 }
