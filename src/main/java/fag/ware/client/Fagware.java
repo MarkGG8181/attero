15bbcb6ee1a8c4896300720082b0c937dc888fc9
linuxdistribution <124876844+linuxdistribution@users.noreply.github.com>
2025-05-20 13:57:45 +0200

    Fabric environment -> client

--- src/main/java/fag/ware/client/Fagware.java
+++ src/main/java/fag/ware/client/Fagware.java
@@ -5,21 +5,22 @@ import fag.ware.client.event.data.EventBus;
 import fag.ware.client.tracker.impl.*;
 import fag.ware.client.util.FileUtil;
 import fag.ware.client.util.music.YtDlpDownloader;
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
-public final class Fagware implements ModInitializer {
+public final class Fagware implements ClientModInitializer {
+
     public static final String MOD_ID = "fagware";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     public static final Fagware INSTANCE = new Fagware();
     public static final EventBus BUS = new EventBus();
 
     @Override
-    public void onInitialize() {
+    public void onInitializeClient() {
         FileUtil.createDir(MOD_ID);
 
         new Thread(() -> {
