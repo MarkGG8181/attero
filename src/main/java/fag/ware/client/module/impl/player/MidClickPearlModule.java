115dcce15342b3ae571db4ba8d1012e23e650fce
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-05-21 21:03:39 +0300

    make MidClickPearl use MiddleClickEvent

--- src/main/java/fag/ware/client/module/impl/player/MidClickPearlModule.java
+++ src/main/java/fag/ware/client/module/impl/player/MidClickPearlModule.java
@@ -1,18 +1,18 @@
 package fag.ware.client.module.impl.player;
 
 import fag.ware.client.event.data.Subscribe;
-import fag.ware.client.event.impl.TickEvent;
+import fag.ware.client.event.impl.MiddleClickEvent;
 import fag.ware.client.module.AbstractModule;
 import fag.ware.client.module.data.ModuleCategory;
 import fag.ware.client.module.data.ModuleInfo;
 import fag.ware.client.module.data.setting.impl.BooleanSetting;
 import fag.ware.client.module.data.setting.impl.NumberSetting;
+import fag.ware.client.tracker.impl.ModuleTracker;
 import fag.ware.client.util.game.InventoryUtil;
 import fag.ware.client.util.math.Timer;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.util.Hand;
-import org.lwjgl.glfw.GLFW;
 
 @ModuleInfo(name = "MidClickPearl", category = ModuleCategory.PLAYER, description = "Middle click pearl")
 public class MidClickPearlModule extends AbstractModule {
@@ -24,26 +24,27 @@ public class MidClickPearlModule extends AbstractModule {
     private final Timer switchBackTimer = new Timer();
 
     @Subscribe
-    public void onTick(TickEvent event) {
+    public void onTick(MiddleClickEvent event) {
         if (mc.player == null || mc.world == null) return;
-        if (GLFW.glfwGetMouseButton(mc.getWindow().getHandle(), GLFW.GLFW_MOUSE_BUTTON_MIDDLE) == GLFW.GLFW_PRESS) {
-            if (delayTimer.hasElapsed(delay.toInt(), true)) {
-                int slot = mc.player.getInventory().getSelectedSlot();
-                InventoryUtil.switchToSlot(Items.ENDER_PEARL);
-                Item item = mc.player.getMainHandStack().getItem();
-                if (item.equals(Items.ENDER_PEARL)) {
-                    mc.interactionManager.interactItem(mc.player, Hand.MAIN_HAND);
-                }
-                if (switchBack.getValue() && switchBackTimer.hasElapsed(switchBackDelay.toInt(), true)) {
-                    mc.player.getInventory().setSelectedSlot(slot);
-                }
+
+        if (delayTimer.hasElapsed(delay.toInt(), true)) {
+            int slot = mc.player.getInventory().getSelectedSlot();
+            InventoryUtil.switchToSlot(Items.ENDER_PEARL);
+            Item item = mc.player.getMainHandStack().getItem();
+            if (item.equals(Items.ENDER_PEARL)) {
+                mc.interactionManager.interactItem(mc.player, Hand.MAIN_HAND);
+            }
+
+            if (switchBack.getValue() && switchBackTimer.hasElapsed(switchBackDelay.toInt(), true)) {
+                mc.player.getInventory().setSelectedSlot(slot);
             }
         }
     }
 
     @Override
-    public void onDisable() {
+    public void onEnable() {
         delayTimer.reset();
         switchBackTimer.reset();
+        ModuleTracker.getInstance().getByName("MidClickFriend").setEnabled(false);
     }
 }
