39246b17beda740c04a2fe75fb7b81f9343f0a80
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-05-21 23:10:01 +0300

    ComputeNextCollision event & Jesus collision mode

--- src/main/java/fag/ware/client/module/impl/movement/JesusModule.java
+++ src/main/java/fag/ware/client/module/impl/movement/JesusModule.java
@@ -1,6 +1,7 @@
 package fag.ware.client.module.impl.movement;
 
 import fag.ware.client.event.data.Subscribe;
+import fag.ware.client.event.impl.ComputeNextCollisionEvent;
 import fag.ware.client.event.impl.MotionEvent;
 import fag.ware.client.module.AbstractModule;
 import fag.ware.client.module.data.ModuleCategory;
@@ -8,15 +9,31 @@ import fag.ware.client.module.data.ModuleInfo;
 import fag.ware.client.module.data.setting.impl.BooleanSetting;
 import fag.ware.client.module.data.setting.impl.StringSetting;
 import fag.ware.client.util.game.MovementUtil;
+import net.minecraft.block.Blocks;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShapes;
 
 @SuppressWarnings("ALL")
 @ModuleInfo(name = "Jesus", description = "Allows you traverse water", category = ModuleCategory.MOVEMENT)
 public class JesusModule extends AbstractModule {
-    private final StringSetting mode = new StringSetting("Mode", "Verus", "Verus");
+    private final StringSetting mode = new StringSetting("Mode", "Verus", "Verus", "Collision");
     private final BooleanSetting boost = (BooleanSetting) new BooleanSetting("Speed boost", false).hide(() -> !mode.is("Verus"));
 
     private boolean water;
+
+    @Subscribe
+    public void onComputeCollision(ComputeNextCollisionEvent event) {
+        switch (mode.getValue()) {
+            case "Collision" -> {
+                if (event.getState().getFluidState().isEmpty()) return;
+
+                if ((event.getState().getBlock() == Blocks.WATER | event.getState().getFluidState().getFluid() == Fluids.WATER) && !mc.player.isTouchingWater() && event.getPos().getY() <= mc.player.getY() - 1) {
+                    event.setShape(VoxelShapes.fullCube());
+                }
+            }
+        }
+    }
 
     @Subscribe
     public void onMotion(MotionEvent event) {
