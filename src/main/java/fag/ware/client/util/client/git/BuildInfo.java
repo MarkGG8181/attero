faef3f68f3e1c7f69ba3303bc9b9d3a2d520d96c
linuxdistribution <124876844+linuxdistribution@users.noreply.github.com>
2025-06-10 09:58:29 +0200

    Make use of @Builder in BuildInfo

--- src/main/java/fag/ware/client/util/client/git/BuildInfo.java
+++ src/main/java/fag/ware/client/util/client/git/BuildInfo.java
@@ -1,57 +1,15 @@
 package fag.ware.client.util.client.git;
 
-import lombok.Getter;
+import lombok.Builder;
+import lombok.Data;
 
-@Getter
+@Data
+@Builder
 public class BuildInfo {
-    private String branch;
-    private String version;
-    private int commits;
-    private String commitId;
-    private String commitIdAbbreviation;
-    private String timestamp;
-
-    private BuildInfo() {}
-
-    public static Builder builder() {
-        return new Builder();
-    }
-
-    public static class Builder {
-        private final BuildInfo info = new BuildInfo();
-
-        public Builder branch(String branch) {
-            info.branch = branch;
-            return this;
-        }
-
-        public Builder version(String version) {
-            info.version = version;
-            return this;
-        }
-
-        public Builder commits(int commits) {
-            info.commits = commits;
-            return this;
-        }
-
-        public Builder commitId(String commitId) {
-            info.commitId = commitId;
-            return this;
-        }
-
-        public Builder commitIdAbbreviation(String commitIdAbbreviation) {
-            info.commitIdAbbreviation = commitIdAbbreviation;
-            return this;
-        }
-
-        public Builder timestamp(String timestamp) {
-            info.timestamp = timestamp;
-            return this;
-        }
-
-        public BuildInfo build() {
-            return info;
-        }
-    }
+    private final String branch;
+    private final String version;
+    private final int commits;
+    private final String commitId;
+    private final String commitIdAbbreviation;
+    private final String timestamp;
 }
