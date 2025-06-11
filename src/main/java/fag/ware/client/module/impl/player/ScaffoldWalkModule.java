0228e8b054ae14ecc8fd95accd66d9774a33ca3d
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-06-11 09:32:48 +0300

    start scaffold recode

--- src/main/java/fag/ware/client/module/impl/player/ScaffoldWalkModule.java
+++ src/main/java/fag/ware/client/module/impl/player/ScaffoldWalkModule.java
@@ -2,194 +2,73 @@ package fag.ware.client.module.impl.player;
 
 import fag.ware.client.event.data.Subscribe;
 import fag.ware.client.event.impl.player.MotionEvent;
-import fag.ware.client.event.impl.player.SprintEvent;
 import fag.ware.client.event.impl.game.TickEvent;
 import fag.ware.client.module.AbstractModule;
 import fag.ware.client.module.data.ModuleCategory;
 import fag.ware.client.module.data.ModuleInfo;
-import fag.ware.client.module.data.setting.impl.BooleanSetting;
-import fag.ware.client.module.data.setting.impl.NumberSetting;
-import fag.ware.client.module.data.setting.impl.StringSetting;
+import fag.ware.client.module.data.setting.impl.GroupSetting;
+import fag.ware.client.module.data.setting.impl.RangeNumberSetting;
 import fag.ware.client.util.game.*;
-import fag.ware.client.util.math.Timer;
-import net.minecraft.block.AirBlock;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.BlockItem;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
 
 @ModuleInfo(name = "ScaffoldWalk", description = "Places blocks under you", category = ModuleCategory.PLAYER)
 public class ScaffoldWalkModule extends AbstractModule {
-    private final StringSetting rotationMode = new StringSetting("Rotation mode", "Simple", "Simple", "Godbridge");
-    private final StringSetting towerMode = new StringSetting("Tower mode", "None", "None", "Motion");
-    private final StringSetting sprintMode = new StringSetting("Sprint mode", "None", "None", "Always", "Manual");
-    private final StringSetting sneakMode = new StringSetting("Sneak mode", "None", "None", "Eagle", "Always");
-    private final StringSetting itemSpoof = new StringSetting("Item spoof", "Switch", "Switch", "None");
 
-    private final BooleanSetting keepY = new BooleanSetting("Keep Y", false);
+    private final GroupSetting rotationGroup = new GroupSetting("Rotations", false);
+    private final RangeNumberSetting speed = (RangeNumberSetting) new RangeNumberSetting("Speed", 10, 180, 10, 180).setParent(rotationGroup);
 
-    private final NumberSetting jumpDistance = (NumberSetting) new NumberSetting("Jump distance", 7, 1, 10)
-            .hide(() -> !rotationMode.is("Godbridge"));
-    private final NumberSetting sneakDelay = (NumberSetting) new NumberSetting("Sneak delay", 57, 0, 300)
-            .hide(() -> sneakMode.is("None") || sneakMode.is("Always"));
-    private final NumberSetting unSneakDelay = (NumberSetting) new NumberSetting("Unsneak delay", 287, 0, 300)
-            .hide(() -> sneakMode.is("None") || sneakMode.is("Always"));
-
-    private final Timer unSneakCounter = new Timer(), sneakCounter = new Timer();
-    private final DistanceCounter distCounter = new DistanceCounter();
-    private float[] lastRots, rots;
-    private double posY;
-
-    private BlockCache cache;
+    @Override
+    public void onEnable() {
+        if (mc.player == null) return;
+        InventoryUtil.switchToBestSlotWithBlocks();
+    }
 
     @Subscribe
     public void onTick(TickEvent event) {
-        if (mc.player != null) {
-            checkForBlocks();
+        if (mc.player == null || mc.world == null) return;
 
-            if (sprintMode.getValue().equals("Always")) {
-                mc.options.sprintKey.setPressed(true);
-            }
+        checkForBlocks();
 
-            if (mc.player.getActiveItem() == null) {
-                switch (itemSpoof.getValue()) {
-                    case "Switch":
-                        InventoryUtil.switchToBestSlotWithBlocks();
-                        break;
+        if (mc.player.getMainHandStack().getItem() instanceof BlockItem item && InventoryUtil.isGoodBlock(item)) {
+            BlockPos belowPlayer = mc.player.getBlockPos().down();
+
+            // Check if player is on ground and walking off the edge
+            if (mc.player.isOnGround() && !mc.world.getBlockState(belowPlayer).isSolidBlock(mc.world, belowPlayer)) {
+                placeBlock(belowPlayer);
+
+                if (MovementUtil.isMoving() && mc.options.jumpKey.isPressed()) {
+                    return;
                 }
-            }
 
-            if (!MovementUtil.isMoving() && mc.options.jumpKey.isPressed() && !mc.options.jumpKey.wasPressed()) {
-                switch (towerMode.getValue()) {
-                    case "Motion":
-                        MovementUtil.setMotionY(0.42);
-                        break;
-                }
+                placeBlock(belowPlayer);
             }
-
-            distCounter.tick(mc.player);
-            if (rotationMode.is("Godbridge")) {
-                if (distCounter.getTravelled() >= jumpDistance.toDouble()) {
-                    if (mc.player.isOnGround()) {
-                        mc.player.jump();
-                    }
-                    distCounter.reset();
-                }
-            }
-
-            if (mc.player.getY() < posY || (!mc.player.isOnGround() && !MovementUtil.isMoving()) || mc.player.getY() - posY > 6 || !keepY.getValue()) {
-                posY = mc.player.getY() - 0.9;
-            }
-            BlockPos playerBlockPos = new BlockPos((int) mc.player.getX(), (int) posY, (int) mc.player.getZ());
-            cache = BlockCache.getCache(playerBlockPos);
         }
     }
 
     @Subscribe
     public void onMotion(MotionEvent event) {
-        if (mc.player == null || !event.isPre()) {
-            return;
-        }
+        if (mc.player.getMainHandStack().getItem() instanceof BlockItem item && InventoryUtil.isGoodBlock(item)) {
+            BlockPos belowPlayer = mc.player.getBlockPos().down();
+            Vec3d lookAt = new Vec3d(belowPlayer.getX() + 0.5, belowPlayer.getY() + 0.5, belowPlayer.getZ() + 0.5);
 
-        switch (sneakMode.getValue()) {
-            case "Eagle":
-                if (mc.world.getBlockState(new BlockPos((int) mc.player.getX(), (int) (mc.player.getY() - 1.0), (int) mc.player.getZ())).getBlock() instanceof AirBlock && mc.player.isOnGround()) {
-                    if (sneakCounter.hasElapsed(sneakDelay.getValue().longValue(), true)) {
-                        mc.options.sneakKey.setPressed(true);
-                    }
-                } else {
-                    if (unSneakCounter.hasElapsed(unSneakDelay.getValue().longValue(), true)) {
-                        mc.options.sneakKey.setPressed(false);
-                    }
-                }
-                break;
-            case "Always":
-                mc.options.sneakKey.setPressed(true);
-                break;
-        }
-
-        if (mc.player != null && mc.player.getActiveItem() != null && cache != null) {
-            ItemStack heldItemStack = mc.player.getActiveItem();
-
-            if (rots != null && heldItemStack != null) {
-                BlockHitResult result = new BlockHitResult(cache.getVec3d(), cache.facing(), cache.pos(), false);
-                if (mc.interactionManager.interactBlock(mc.player, Hand.MAIN_HAND, result).isAccepted()) {
-                    mc.player.swingHand(Hand.MAIN_HAND);
-                }
-            }
-        }
-
-        if (cache != null && lastRots != null) {
-            switch (rotationMode.getValue()) {
-                case "Simple":
-                    rots = RotationUtil.getSimpleRotations(cache, lastRots);
-                    break;
-
-                case "Godbridge":
-                    rots = RotationUtil.getGodbridgeRotations(cache, lastRots);
-                    break;
-            }
-
+            float[] rots = RotationUtil.toRotation(lookAt, speed.getMinAsFloat(), speed.getMaxAsFloat());
             event.setYaw(rots[0]);
             event.setPitch(rots[1]);
-            lastRots = new float[]{event.getYaw(), event.getPitch()};
-        } else {
-            if (lastRots != null) {
-                rots = lastRots;
-
-                event.setYaw(rots[0]);
-                event.setPitch(rots[1]);
-                lastRots = new float[]{event.getYaw(), event.getPitch()};
-            }
         }
     }
 
-    @Subscribe
-    public void onSprint(SprintEvent event) {
-        if (sprintMode.is("None") && rots != null) {
-            event.setCancelled(true);
+    private void placeBlock(BlockPos pos) {
+        assert mc.player != null;
+        if (mc.player.getMainHandStack().getItem() instanceof BlockItem) {
+            assert mc.interactionManager != null;
+            mc.interactionManager.interactBlock(mc.player, Hand.MAIN_HAND, new BlockHitResult(new Vec3d(pos.getX(), pos.getY(), pos.getZ()), Direction.UP, pos, false));
+            mc.player.swingHand(Hand.MAIN_HAND);
         }
-    }
-
-    @Override
-    public void onInit() {
-        rotationMode.onChange(val -> {
-            if (val.equals("Godbridge")) {
-                keepY.setValue(false);
-                sneakMode.setValue("None");
-            }
-        });
-    }
-
-    @Override
-    public void onEnable() {
-        if (mc.player == null) {
-            return;
-        }
-
-        checkForBlocks();
-
-        switch (itemSpoof.getValue()) {
-            case "Switch":
-                InventoryUtil.switchToBestSlotWithBlocks();
-                break;
-        }
-
-        lastRots = new float[]{RotationUtil.getAdjustedYaw(), 90};
-        posY = mc.player.getY() - 0.9;
-    }
-
-    @Override
-    public void onDisable() {
-        mc.options.sneakKey.setPressed(false);
-
-        distCounter.reset();
-        unSneakCounter.reset();
-    }
-
-    @Override
-    public String getSuffix() {
-        return rotationMode.getValue();
     }
 
     private void checkForBlocks() {
