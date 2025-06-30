5303c64a11fc358cadd4e35ed71e94b1513fc4c6
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-07-01 00:01:48 +0300

    fix: semi fix for body yaw

--- src/main/java/io/github/client/mixin/LivingEntityRendererMixin.java
+++ src/main/java/io/github/client/mixin/LivingEntityRendererMixin.java
@@ -2,9 +2,7 @@ package io.github.client.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import io.github.client.tracker.impl.RotationTracker;
-import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
-import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.client.render.entity.state.LivingEntityRenderState;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.math.MathHelper;
@@ -14,33 +12,43 @@ import org.spongepowered.asm.mixin.injection.At;
 import static io.github.client.util.java.interfaces.IMinecraft.mc;
 
 /**
- * @author kibty
- * fixed rots being only locked to a target and body yaw - mark
+ * @author markuss
  */
 @Mixin(LivingEntityRenderer.class)
-public abstract class LivingEntityRendererMixin<S extends LivingEntityRenderState, M extends EntityModel<? super S>> {
+public abstract class LivingEntityRendererMixin<S extends LivingEntityRenderState> {
     @ModifyExpressionValue(method = "updateRenderState(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/client/render/entity/state/LivingEntityRenderState;F)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/LivingEntityRenderer;clampBodyYaw(Lnet/minecraft/entity/LivingEntity;FF)F"))
     private float changeBodyYaw(final float original, final LivingEntity living, final S state, final float tickDelta) {
-        if (living != mc.player || mc.currentScreen instanceof InventoryScreen)
+        if (living != mc.player)
             return original;
 
-        final float lerpedBodyYaw = MathHelper.lerpAngleDegrees(tickDelta, living.lastHeadYaw, living.headYaw);
-        return LivingEntityRenderer.clampBodyYaw(living, lerpedBodyYaw, tickDelta);
+        if (RotationTracker.rotating) {
+            return MathHelper.lerpAngleDegrees(tickDelta, RotationTracker.prevYaw, RotationTracker.yaw);
+        }
+
+        return original;
     }
 
     @ModifyExpressionValue(method = "updateRenderState(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/client/render/entity/state/LivingEntityRenderState;F)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;lerpAngleDegrees(FFF)F"))
     private float changeHeadYaw(final float original, final LivingEntity living, final S state, final float tickDelta) {
-        if (living != mc.player || mc.currentScreen instanceof InventoryScreen)
+        if (living != mc.player)
             return original;
 
-        return MathHelper.lerpAngleDegrees(tickDelta, RotationTracker.INSTANCE.prevYaw, RotationTracker.INSTANCE.yaw);
+        if (RotationTracker.rotating) {
+            return MathHelper.lerpAngleDegrees(tickDelta, RotationTracker.prevYaw, RotationTracker.yaw);
+        }
+
+        return original;
     }
 
     @ModifyExpressionValue(method = "updateRenderState(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/client/render/entity/state/LivingEntityRenderState;F)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;getLerpedPitch(F)F"))
     private float changePitch(final float original, final LivingEntity living, final S state, final float tickDelta) {
-        if (living != mc.player || mc.currentScreen instanceof InventoryScreen)
+        if (living != mc.player)
             return original;
 
-        return MathHelper.lerpAngleDegrees(tickDelta, RotationTracker.INSTANCE.prevPitch, RotationTracker.INSTANCE.pitch);
+        if (RotationTracker.rotating) {
+            return MathHelper.lerpAngleDegrees(tickDelta, RotationTracker.prevPitch, RotationTracker.pitch);
+        }
+
+        return original;
     }
 }
