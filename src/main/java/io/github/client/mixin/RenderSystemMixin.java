d2f5219eb5aa11e041179282b11d3891d14920d8
ayaxperson <danielthegreat@disroot.org>
2025-07-05 15:31:13 +0200

    Fixed potential rotation desync issue

---
+++ src/main/java/io/github/client/mixin/RenderSystemMixin.java
@@ -0,0 +1,19 @@
+package io.github.client.mixin;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import io.github.client.Attero;
+import io.github.client.event.impl.render.FlipFrameEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(RenderSystem.class)
+public class RenderSystemMixin {
+
+    @Inject(method = "flipFrame", at = @At("HEAD"))
+    private static void onFlipFrame(final CallbackInfo ci) {
+        Attero.BUS.post(new FlipFrameEvent());
+    }
+
+}
--- src/main/java/io/github/client/tracker/impl/RotationTracker.java
+++ src/main/java/io/github/client/tracker/impl/RotationTracker.java
@@ -2,11 +2,15 @@ package io.github.client.tracker.impl;
 
 import io.github.client.event.data.Subscribe;
 import io.github.client.event.impl.player.MotionEvent;
+import io.github.client.event.impl.player.ReceivePacketEvent;
+import io.github.client.event.impl.render.FlipFrameEvent;
 import io.github.client.module.data.rotate.AbstractRotator;
 import io.github.client.tracker.AbstractTracker;
 import io.github.client.util.java.interfaces.IMinecraft;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
-import net.minecraft.util.math.MathHelper;
+import net.minecraft.network.packet.s2c.play.EntityPositionS2CPacket;
+import net.minecraft.network.packet.s2c.play.EntityS2CPacket;
+import net.minecraft.network.packet.s2c.play.PlayerPositionLookS2CPacket;
 
 import java.util.Comparator;
 
@@ -18,43 +22,100 @@ public class RotationTracker extends AbstractTracker<AbstractRotator> implements
     public static float yaw, pitch;
     public static boolean rotating;
 
+    private static float nextYaw, nextPitch;
+    private static boolean hasNextRotation = false;
+
+    @Subscribe
+    private void onFlipFrame(final FlipFrameEvent flipFrameEvent) {
+        if (mc.player == null || mc.world == null) {
+            hasNextRotation = false;
+            nextYaw = -1;
+            nextPitch = -1;
+
+            return;
+        }
+
+        var best = list.stream()
+                .filter(AbstractRotator::isEnabled)
+                .max(Comparator.comparingInt(a -> a.priority))
+                .orElse(null);
+
+        for (var rotator : list) {
+            if (rotator == best) continue;
+            rotator.canPerform = false;
+        };
+
+        if (best != null) {
+            best.canPerform = true;
+            float[] target = best.shouldRotate();
+            if (target != null) {
+                hasNextRotation = true;
+                nextYaw = target[0];
+                nextPitch = target[1];
+
+                return;
+            }
+        }
+
+        hasNextRotation = false;
+        nextYaw = -1;
+        nextPitch = -1;
+    }
+
     @Subscribe
     private void onMotion(MotionEvent event) {
         if (event.isPre()) {
-            if (rotating && mc.currentScreen instanceof InventoryScreen) {
+            if (hasNextRotation && mc.currentScreen instanceof InventoryScreen) {
+                hasNextRotation = false;
+            }
+
+            if (hasNextRotation) {
+                prevYaw = yaw;
+                prevPitch = pitch;
+
+                event.yaw = nextYaw;
+                event.pitch = nextPitch;
+
+                yaw = event.yaw;
+                pitch = event.pitch;
+
+                rotating = true;
+            } else {
                 rotating = false;
-                return;
+
+                // Not really needed as event isn't modified in this case
+                // but still changed as to not desync prevYaw & prevPitch
+                prevYaw = yaw;
+                prevPitch = pitch;
+
+                yaw = event.yaw;
+                pitch = event.pitch;
             }
+        }
+    }
 
-            var best = list.stream()
-                    .filter(AbstractRotator::isEnabled)
-                    .max(Comparator.comparingInt(a -> a.priority))
-                    .orElse(null);
+    @Subscribe
+    private void onPacket(final ReceivePacketEvent receivePacketEvent) {
+        switch (receivePacketEvent.packet) {
+            case PlayerPositionLookS2CPacket packet -> hasNextRotation = false;
 
-            for (var rotator : list) {
-                if (rotator == best) continue;
-                rotator.canPerform = false;
-            }
-
-            if (best != null) {
-                best.canPerform = true;
-                float[] target = best.shouldRotate();
-                if (target != null) {
-                    rotating = true;
-
-                    prevYaw = yaw;
-                    prevPitch = pitch;
-
-                    yaw = MathHelper.wrapDegrees(target[0]);
-                    pitch = target[1];
-
-                    event.yaw = yaw;
-                    event.pitch = pitch;
+            case EntityPositionS2CPacket packet -> {
+                if (mc.player == null || mc.world == null)
                     return;
-                }
+
+                if (mc.world.getEntityById(packet.entityId()) == mc.player)
+                    hasNextRotation = false;
             }
 
-            rotating = false;
+            case EntityS2CPacket packet -> {
+                if (mc.player == null || mc.world == null)
+                    return;
+
+                if (packet.getEntity(mc.world) == mc.player)
+                    hasNextRotation = false;
+            }
+
+            default -> { }
         }
     }
 }
